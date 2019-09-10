a16d7cf5f92379e2c7179d4e1d50431269eeba49
andie5 <andieasa@googlemail.com>
2019-09-10 21:19:14 +0100

    Render nasa images using classes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = { 
+    nasaData: null,
+    error: null
+   }
+
+  componentDidMount() {
+    fetch("https://images-api.nasa.gov/search?q=apollo%2011")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            nasaData: result
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            error
+          });
+        }
+      )
+  }
+
+  render() {
+
+    return (
+      <div>
+        {this.state.nasaData && this.state.nasaData.collection.items.map(item => {
+          if(!item.links) return null;
+          const imageLink = item.links.find((link) => link.render === "image")
+          return <img src={imageLink.href} alt={item.data[0].description}/>
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
