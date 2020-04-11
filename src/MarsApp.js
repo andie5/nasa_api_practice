0b81aab1fd439cf221f42e0dde4692109bc01f3a
andie5 <andieasa@googlemail.com>
2020-04-11 19:29:29 +0100

    re-added asynchronous call

--- src/MarsApp.js
+++ src/MarsApp.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 const API_KEY = "1XT0wLSE6HyidMviha2OgmPVSryzhJPZHy4FQvOW";
 
 const MarsData = () => {
-  // cAN DEFINE DEFAULT VALE IN USEsTATE and pass null
+  // We can define a default state in usestate and use null
   const [data, setData] = useState();
 
   //   useEffect(() => {
@@ -28,7 +28,7 @@ const MarsData = () => {
         );
 
         //   Response json also returns a promise so this is why we have to wait for it - i.e. await
-        const dataResponse = response.json();
+        const dataResponse = await response.json();
 
         setData(dataResponse);
       } catch (error) {
